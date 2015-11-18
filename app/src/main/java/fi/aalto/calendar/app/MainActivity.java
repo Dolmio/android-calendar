491db6c4acae2fe083b340dda3746eeea537588b
Juho Salmio <juho.salmio@gmail.com>
2015-11-18 19:12:28 +0200

    make add event and sync toolbar buttons work

--- app/src/main/java/fi/aalto/calendar/app/MainActivity.java
+++ app/src/main/java/fi/aalto/calendar/app/MainActivity.java
@@ -2,27 +2,31 @@ package fi.aalto.calendar.app;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.month_pager);
-
         ViewPager pager = (ViewPager)findViewById(R.id.month_pager);
         MonthPageAdapter adapter = new MonthPageAdapter(getSupportFragmentManager());
         pager.setAdapter(adapter);
         pager.setCurrentItem(adapter.getInitialPosition());
-    }
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        new MenuInflater(this).inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem add_menu) {
@@ -30,6 +34,10 @@ public class MainActivity extends FragmentActivity {
             case R.id.add_menu:
                 Intent intent = new Intent(MainActivity.this, EditActivity.class);
                 startActivity(intent);
+                return true;
+            case R.id.sync_menu:
+                Intent intent2 = new Intent(MainActivity.this, SyncActivity.class);
+                startActivity(intent2);
                 return true;
             default:
                 return super.onOptionsItemSelected(add_menu);
